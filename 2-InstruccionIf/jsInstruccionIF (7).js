b7d3a0a77ea84019b60e0d21185322a4943f24fd
Crouzers <lapijadealf@gmail.com>
2018-09-24 17:42:22 -0300

    ahí están el 5, 6 y 7 corregidos y termina3

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,9 +2,13 @@ function Mostrar()
 {
 //tomo la edad  
 
-	var num = parseInt(document.getElementById("edad").value)
+	var num = parseInt(document.getElementById("edad").value);
+	var estado = (document.getElementById("estadoCivil").value);
 
-	if((document.getElementById("estadoCivil").value)==)
+	if((num<=17) && (estado == ("Casado" || "Divorciado")));
+	{
+		alert("usted es muy joven para no ser soltero")
+	}
 
 
 
