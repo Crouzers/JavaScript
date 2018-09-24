be336fe8312c27f1fe9d2881b1a9f238da5754ac
Crouzers <lapijadealf@gmail.com>
2018-09-24 20:00:44 -0300

    ahi hice el 8 pero el ya no me acuerdo como hacerlo, se que lo explicaste, pero no que comandos usar

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,12 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+var num = parseInt(document.getElementById("edad").value);
+var estado = (document.getElementById("estadoCivil").value);
 
+	if ((num >=18)&&(estado == "Soltero"))
+	{
+		alert("no es menor de edad y es soltero")
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,6 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	alert("no se que mierda hacer aca lol")
 
 }//FIN DE LA FUNCIÓN
